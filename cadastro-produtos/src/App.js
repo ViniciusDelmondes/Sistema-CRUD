ecf3b9ed1cd5f6b9e56b3b604ba2e02996f76e0d
Vinicius Delmondes <viniciusdelmo@hotmail.com>
2020-04-28 14:59:55 -0300

    Sistema CRUD

---
+++ cadastro-produtos/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import NavBar from './components/navbar.js'
+import Rotas from './rotas'
+
+import { HashRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <HashRouter>
+      <div className="container">
+        <NavBar />
+        <Rotas />
+      </div>
+    </HashRouter>
+  );
+}
+
+export default App;
